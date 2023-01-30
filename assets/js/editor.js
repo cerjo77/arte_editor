8f2616ef7da58bf02ba2b6b95c6ef8ad4c612dbf
Sergio Baumgarten <cerjo77@gmail.com>
2023-01-30 17:54:19 -0300

    correção do função de aspect ratio e draggable

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,23 +1,57 @@
-const div = document.getElementById("arte");
-const rect = div.getBoundingClientRect();
-let divWidth = rect.width;
-let divHeight = rect.height;
-
 function heightResize(){
+    const div = document.getElementById("arte");
+    const rect = div.getBoundingClientRect();
+    let divWidth = rect.width;
+    let divHeight = rect.height;
+
     div.style.height = `${divWidth - (divWidth*0.55)}px`;
     document.getElementById("logText").innerText = `${divWidth - (divWidth*0.55)}`;
 }
 
-function throttle(fn, wait) {
-    let time = Date.now();
-    return function() {
-      if ((time + wait - Date.now()) < 0) {
-        fn();
-        time = Date.now();
-      }
-    }
+heightResize();
+window.addEventListener('resize', heightResize);
+
+const draggable = document.querySelector(".draggable");
+let isDragging = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+
+draggable.addEventListener("mousedown", dragStart);
+draggable.addEventListener("mouseup", dragEnd);
+draggable.addEventListener("mouseout", dragEnd);
+draggable.addEventListener("mousemove", drag);
+
+function dragStart(e) {
+  initialX = e.clientX - xOffset;
+  initialY = e.clientY - yOffset;
+
+  isDragging = true;
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+
+  isDragging = false;
+}
+
+function drag(e) {
+  if (isDragging) {
+    e.preventDefault();
+    currentX = e.clientX - initialX;
+    currentY = e.clientY - initialY;
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, draggable);
   }
+}
 
-//heightResize();
-
-window.addEventListener("resize", throttle(heightResize,100));
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
